9859e46758b51bfccae512f21579d3dca50ae1cd
あさぴっぴ1020 <88693257+Asapi1020@users.noreply.github.com>
2024-10-15 18:42:40 +0900

    🎉 statusチェックのAPI

---
+++ api/status.ts
@@ -0,0 +1,6 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+	console.log({ req });
+	res.status(200).json({ status: "OK" });
+}
