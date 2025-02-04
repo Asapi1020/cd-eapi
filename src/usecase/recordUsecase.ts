5df722b2220df59e3f4027c1dbe486b0f65ceafe
あさぴっぴ1020 <88693257+Asapi1020@users.noreply.github.com>
2025-02-04 21:54:51 +0900

    🔊 デバッグログの追加

--- src/usecase/recordUsecase.ts
+++ src/usecase/recordUsecase.ts
@@ -38,13 +38,18 @@ export async function postRequestToRecord(
 		// change steamID from 32 to 64
 		const ids: string[] = [];
 		for (const stat of request.userStats) {
+			console.log(stat.steamID);
 			const steam32ID = Number.parseInt(stat.steamID);
+			console.log(steam32ID);
 			const steam64ID = steam32ID + 76561197960265728;
+			console.log(steam64ID);
 			stat.steamID = steam64ID.toString();
+			console.log(stat.steamID);
 			ids.push(stat.steamID);
 		}
 
 		const players = await steamAPIClient.getPlayerSummaries(ids);
+		console.log(players);
 		const playerMap = new Map(
 			players.map((player) => [player.id, player.name]),
 		);
